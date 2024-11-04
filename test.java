58663c590f0d0feee0b4c8cb2fb9df65e292b001
karthick-10 <karthick.r@brimmatech.com>
2024-11-04 11:37:53 +0530

    commited file in first branch

--- test.java
+++ test.java
@@ -1,5 +1,5 @@
 public class test {
     public static void main(String[] args) {
-        System.out.print("hello from main branch");
+        System.out.print("hello from first-branch");
     }
 }
